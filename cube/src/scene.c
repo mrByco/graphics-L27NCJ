b0c652c65cde7dc61db1822a41ababa08e3889d3
FIRST_NAME LAST_NAME <FIRST_NAME LAST_NAME>
2023-04-06 12:47:29 +0200

    dino

---
+++ cube/src/scene.c
@@ -0,0 +1,111 @@
+#include "scene.h"
+
+#include <obj/load.h>
+#include <obj/draw.h>
+
+float* originalZ;
+int multiplierDir = -1;
+float multiplier = 0;
+
+
+void init_scene(Scene* scene)
+{
+    load_model(&(scene->cube), "assets/models/raptor.obj");
+    scene->texture_id = load_texture("assets/textures/raptor.png");
+    
+    originalZ = malloc(sizeof(float) * scene->cube.n_normals);
+    for (int i = 0; i < scene->cube.n_vertices; i++){
+        originalZ[i] = scene->cube.vertices[i].z;
+    }
+
+    glBindTexture(GL_TEXTURE_2D, scene->texture_id);
+
+    scene->material.ambient.red = 0.0;
+    scene->material.ambient.green = 0.0;
+    scene->material.ambient.blue = 0.0;
+
+    scene->material.diffuse.red = 1.0;
+    scene->material.diffuse.green = 1.0;
+    scene->material.diffuse.blue = 0.0;
+
+    scene->material.specular.red = 0.0;
+    scene->material.specular.green = 0.0;
+    scene->material.specular.blue = 0.0;
+
+    scene->material.shininess = 0.0;
+}
+
+void set_lighting()
+{
+    float ambient_light[] = { 0.0f, 0.0f, 0.0f, 1.0f };
+    float diffuse_light[] = { 1.0f, 1.0f, 1.0, 1.0f };
+    float specular_light[] = { 0.0f, 0.0f, 0.0f, 1.0f };
+    float position[] = { 0.0f, 0.0f, 10.0f, 1.0f };
+
+    glLightfv(GL_LIGHT0, GL_AMBIENT, ambient_light);
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, diffuse_light);
+    glLightfv(GL_LIGHT0, GL_SPECULAR, specular_light);
+    glLightfv(GL_LIGHT0, GL_POSITION, position);
+}
+
+void set_material(const Material* material)
+{
+    float ambient_material_color[] = {
+        material->ambient.red,
+        material->ambient.green,
+        material->ambient.blue
+    };
+
+    float diffuse_material_color[] = {
+        material->diffuse.red,
+        material->diffuse.green,
+        material->diffuse.blue
+    };
+
+    float specular_material_color[] = {
+        material->specular.red,
+        material->specular.green,
+        material->specular.blue
+    };
+
+    glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, ambient_material_color);
+    glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, diffuse_material_color);
+    glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, specular_material_color);
+
+    glMaterialfv(GL_FRONT_AND_BACK, GL_SHININESS, &(material->shininess));
+}
+
+void update_scene(Scene* scene)
+{
+    multiplier += 0.002;
+    for (int i = 0; i < scene->cube.n_vertices; i++){
+        scene->cube.vertices[i].z = originalZ[i] * multiplier;
+    }
+}
+
+void render_scene(const Scene* scene)
+{
+    set_material(&(scene->material));
+    set_lighting();
+    draw_origin();
+    draw_model(&(scene->cube));
+}
+
+void draw_origin()
+{
+    glBegin(GL_LINES);
+
+    glColor3f(1, 0, 0);
+    glVertex3f(0, 0, 0);
+    glVertex3f(1, 0, 0);
+
+    glColor3f(0, 1, 0);
+    glVertex3f(0, 0, 0);
+    glVertex3f(0, 1, 0);
+
+    glColor3f(0, 0, 1);
+    glVertex3f(0, 0, 0);
+    glVertex3f(0, 0, 1);
+
+    glEnd();
+}
